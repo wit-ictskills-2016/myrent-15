c9a5025f531cac931374c1419ca456694af55efc
John Fitzgerald <usplitu@hotmail>
2016-10-16 12:08:07 +0100

    in progress - cleaning up permissions

--- app/src/main/java/org/wit/myrent/activities/ResidenceFragment.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceFragment.java
@@ -234,33 +234,14 @@ public class ResidenceFragment extends Fragment implements TextWatcher,
   }
 
   public void checkContactsReadPermission() {
-    // Here, getActivity() is the current activity
     if (ContextCompat.checkSelfPermission(getActivity(),
         Manifest.permission.READ_CONTACTS)
         != PackageManager.PERMISSION_GRANTED) {
-
-      // Should we show an explanation?
-      if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
-          Manifest.permission.READ_CONTACTS)) {
-
-        // Show an expanation to the user *asynchronously* -- don't block
-        // this thread waiting for the user's response! After the user
-        // sees the explanation, try again to request the permission.
-        info(this, "shouldShowRequestPermissionRationale");
-
-      } else {
-
-        // No explanation needed, we can request the permission.
 
         ActivityCompat.requestPermissions(
             getActivity(),
             new String[]{Manifest.permission.READ_CONTACTS},
             REQUEST_CONTACT);
-
-        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-        // app-defined int constant. The callback method gets the
-        // result of the request.
       }
     }
-  }
 }
